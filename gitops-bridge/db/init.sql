84a6798f690b86fd90c5932a572da5cef06ef66f
Ludger Pottmeier <ludger.pottmeier@gmail.com>
2025-03-23 13:35:39 +0100

    keys

--- gitops-bridge/db/init.sql
+++ gitops-bridge/db/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE testdb;
 
 
 CREATE TABLE employee (
-    id serial4 NOT NULL,
+    id integer NOT NULL,
     first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
     email varchar(255) NOT NULL,
